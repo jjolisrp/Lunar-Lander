4b633b46275cb6020e269061af0ae86538ee93cd
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-10-03 11:01:59 +0200

    Starting Project

--- sketch.js
+++ sketch.js
@@ -1,9 +1,29 @@
 function setup() 
 {
-	createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
-function draw()
+
+
+function draw() 
 {
-    
+
+}
+
+class Mover 
+{
+    constructor(m, x, y, c) 
+    {
+      this.mass = m;
+      this.position = createVector(x, y);
+      this.velocity = createVector(0, 0);
+      this.acceleration = createVector(0, 0);
+      this.color = c;
+    }
+}
+
+applyForce(force) 
+{
+    let f = p5.Vector.div(force, this.mass);
+    this.acceleration.add(f);
 }
