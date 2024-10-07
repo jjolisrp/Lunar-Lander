7e28830be578788a35a3caf7ab4474a291932b34
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-10-07 10:02:35 +0200

    Changes to sketch and added a resource folder

--- sketch.js
+++ sketch.js
@@ -1,29 +1,42 @@
 function setup() 
 {
   createCanvas(windowWidth, windowHeight);
+
+  background(200);
+
+  InitializeNave();
 }
-
-
 
 function draw() 
 {
-
+  
 }
 
-class Mover 
+class Nave 
 {
-    constructor(m, x, y, c) 
+    constructor(m, x, y, img) 
     {
       this.mass = m;
       this.position = createVector(x, y);
       this.velocity = createVector(0, 0);
       this.acceleration = createVector(0, 0);
-      this.color = c;
+      // Image(img, this.position.x, this.position.y, 50, 50);
     }
 }
 
-applyForce(force) 
+function InitializeNave()
 {
-    let f = p5.Vector.div(force, this.mass);
-    this.acceleration.add(f);
+  let mass = 0.5;
+  let posX = windowWidth / 4;
+  let posY = windowHeight / 4;
+
+  // img = loadImage("recursos\imagenes\Fondo galaxia.jpg");
+
+  // nave = new Nave(mass, posX, posY, img);
 }
+
+// applyForce(force) 
+// {
+//     let f = p5.Vector.div(force, this.mass);
+//     this.acceleration.add(f);
+// }
