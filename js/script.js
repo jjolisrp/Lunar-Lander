199b8bd192eead0b6584b8d284469bd0449ef108
jjolisrp <165792842+jjolisrp@users.noreply.github.com>
2024-12-16 19:45:12 +0100

    finished options page + starting to use localstorage

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-const musicSlider = document.getElementById('music-slider');
-const musicValue = document.getElementById('music-value');
-
 function ChangePage(page)
 {
     window.location.href = page;
@@ -11,10 +8,38 @@ function WindowClose()
     window.close();
 }
 
+const musicSlider = document.getElementById('music-slider');
+const musicValue = document.getElementById('music-value');
+
 //Función de flecha
 musicSlider.addEventListener('input', () =>
 {
     musicValue.textContent = `${musicSlider.value}%`;
     
-    musicSlider.style.background = `linear-gradient(to right, #00cf68 ${value}%, #00460c ${value}%)`;
+    musicSlider.style.background = `linear-gradient(to right, #00cf68 ${musicSlider.value}%,rgb(134, 134, 134) ${musicSlider.value}%)`;
+
+    localStorage.setItem('MusicVolume', musicSlider.value);
 });
+
+const sfxSlider = document.getElementById('sfx-slider');
+const sfxValue = document.getElementById('sfx-value');
+
+sfxSlider.addEventListener('input', () =>
+{
+    sfxValue.textContent = `${sfxSlider.value}%`
+
+    sfxSlider.style.background = `linear-gradient(to right, #00cf68 ${sfxSlider.value}%,rgb(134, 134, 134) ${sfxSlider.value}%)`;
+
+    localStorage.setItem('SFXVolume', sfxSlider.value);
+});
+
+document.addEventListener('DOMContentLoaded', () =>
+    {
+        const savedMusicValue = localStorage.getItem('MusicVolume');
+        const savedSFXValue = localStorage.getItem('SFXVolume');
+    
+        if(savedMusicValue != null)
+        {
+            
+        }
+    });
